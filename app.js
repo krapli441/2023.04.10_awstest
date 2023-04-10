edc899e0cf0060ccab072f2c92d01c7c38d14151
박준형 <108733795+krapli441@users.noreply.github.com>
2023-04-10 12:08:35 +0900

    간단한 형태의 app.js 서버 파일 제작

--- app.js
+++ app.js
@@ -1,2 +1,33 @@
 import mysql2 from "mysql2";
 import http from "http";
+import fs from "fs";
+
+const server = http.createServer(function (request, response) {
+  if (request.method === "GET") {
+    const url = request.url;
+    let filePath = "." + url;
+
+    if (filePath === "./") {
+      filePath = "./index.html";
+    }
+
+    if (request.url === "/favicon.ico") {
+      return;
+    }
+
+    fs.readFile(filePath, (error, data) => {
+      if (error) {
+        throw error;
+      }
+      if (url.endsWith(".html")) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+      }
+      response.write(data);
+      response.end();
+    });
+  }
+});
+
+server.listen(2080, function () {
+  console.log("server is runnig...");
+});
