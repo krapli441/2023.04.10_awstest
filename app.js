3867a1544b5d8ca99fa6376bc4914b5d50b8edae
박준형 <108733795+krapli441@users.noreply.github.com>
2023-04-10 12:46:16 +0900

    mysql 접속 테스트

--- app.js
+++ app.js
@@ -2,6 +2,25 @@ import mysql2 from "mysql2";
 import http from "http";
 import fs from "fs";
 
+// ? mysql에 접속하기 위해 통신 객체 설정.
+const conn = mysql2.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "VHzmffkr1208",
+  database: "user_info",
+  port: 3306,
+  socketPath: "/tmp/mysql.sock",
+});
+
+// ? mysql 접속 테스트
+conn.connect(function (errors, result, fields) {
+  if (errors) {
+    throw errors;
+  }
+  console.log("mysql is connected");
+});
+
+// ? 서버 객체 설정
 const server = http.createServer(function (request, response) {
   if (request.method === "GET") {
     const url = request.url;
@@ -21,10 +40,14 @@ const server = http.createServer(function (request, response) {
       if (url.endsWith(".html")) {
         res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
       }
-      console.log(data);
       response.write(data);
       response.end();
     });
+  }
+
+  if (request.method === "POST" && request.url.startsWith("/accountSubmit")) {
+    console.log("버튼테스트");
+    response.end();
   }
 });
 
